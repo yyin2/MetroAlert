f37d79a7968f76b8404659f61797fc1568d16dab
yinyan <yinyan192@gmail.com>
2026-01-07 14:50:44 +0800

    Initial Commit

---
+++ MetroAlert/MetroAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetroAlertApp.swift
+//  MetroAlert
+//
+//  Created by Yin Yan on 2026/1/7.
+//
+
+import SwiftUI
+
+@main
+struct MetroAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
